d477da816d35676a8d41c3ae9a039b339a3106f8
sang22102005@gmail.com <sang22102005@gmail.com>
2025-10-14 19:37:04 +0700

    add IsApproved for CourseEntity

--- courses_buynsell_api/Entities/Course.cs
+++ courses_buynsell_api/Entities/Course.cs
@@ -13,7 +13,7 @@ public class Course
     public string Title { get; set; } = String.Empty;
     [Required]
     public string Description { get; set; } = String.Empty;
-    [Required] 
+    [Required]
     public decimal Price { get; set; }
     [MaxLength(50)]
     public string Level { get; set; } = String.Empty;
@@ -25,6 +25,7 @@ public class Course
     public int DurationHours { get; set; }
     public int TotalPurchased { get; set; }
     public decimal AverageRating { get; set; }
+    public bool IsApproved { get; set; }
     [Required]
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
@@ -34,6 +35,6 @@ public class Course
     public Category? Category { get; set; }
     public ICollection<CourseSkill> CourseSkills { get; set; } = new List<CourseSkill>();
     public ICollection<CourseContent> CourseContents { get; set; } = new List<CourseContent>();
-    public ICollection<Review> Reviews { get; set; } = new  List<Review>();
-    public ICollection<TargetLearner> TargetLearners { get; set; } = new  List<TargetLearner>();
+    public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    public ICollection<TargetLearner> TargetLearners { get; set; } = new List<TargetLearner>();
 }
